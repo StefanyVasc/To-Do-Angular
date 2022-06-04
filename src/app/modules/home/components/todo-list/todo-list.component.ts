e1c324586fe915b2845c89f41aebeb5dc8c4617d
Stefany <stefany.vasc.sa@gmail.com>
2022-06-04 20:58:22 -0300

    🚧include input validation and checked style

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,33 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
+  public taskList: Array<TaskList> = [];
 
-  public taskList: Array<TaskList> = [ ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort(
+      (first, last) => Number(first.checked) - Number(last.checked)
+    );
   }
 
   public deleteItemTaskList(index: number) {
-    this.taskList.splice(index,1)
+    this.taskList.splice(index, 1);
   }
 
   public deleteAllTaskList(): void {
-    const confirm = window.confirm("Você realmente deseja deletar tudo?")
+    const confirm = window.confirm('Você realmente deseja deletar tudo?');
 
-    if(confirm) {
-      this.taskList = []
+    if (confirm) {
+      this.taskList = [];
     }
   }
 
   public setEmitTaskList(value: string): void {
-    this.taskList.push({task: value, checked: false })
+    this.taskList.push({ task: value, checked: false });
+  }
+
+  public validationInput(task: string, index: number): void {
+    console.log(task);
+    if (!task.length) {
+      alert('essa task vazia será deletada!');
+      this.deleteItemTaskList(index);
+    }
   }
 }
